1904ba378bcad0d4d5410a97ab80394b4f65a2d4
Mohasin K R <30471994+mohasinkr@users.noreply.github.com>
2023-05-14 18:39:32 +0530

    improve formatting

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,25 +1,23 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import '../App.css';
-import { Link } from 'react-router-dom';
-import { useContext, useState } from 'react';
-import { useUserAuth } from '../context/UserAuthContext';
-import { Alert } from 'react-bootstrap';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "../App.css";
+import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { useUserAuth } from "../context/UserAuthContext";
+import { Alert } from "react-bootstrap";
 
 function Signup() {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error,setError] = useState('');
-  const {signUp} = useUserAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { signUp } = useUserAuth();
 
   const handleSignUp = async (e) => {
     e.preventDefault();
-    setError('');
-    try{
-      await signUp(email,password);
-    }
-    catch(err){
+    setError("");
+    try {
+      await signUp(email, password);
+    } catch (err) {
       setError(err.message);
     }
   };
@@ -28,22 +26,30 @@ function Signup() {
     <>
       <div className="p-4 box">
         <h2 className="mb-3">Firebase Auth Signup</h2>
-        {error && <Alert variant='danger'>{error}</Alert>}
+        {error && <Alert variant="danger">{error}</Alert>}
         <Form onSubmit={handleSignUp}>
           <Form.Group className="mb-3 text-dark" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" onChange={
-              (e) => setEmail(e.target.value)} />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password"
+            <Form.Control
+              type="password"
+              placeholder="Password"
               onChange={(e) => setPassword(e.target.value)}
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="I accept the terms & conditions here." />
+            <Form.Check
+              type="checkbox"
+              label="I accept the terms & conditions here."
+            />
           </Form.Group>
           <div className="d-grid gap-2">
             <Button variant="primary" type="Submit">
@@ -54,12 +60,11 @@ function Signup() {
         </Form>
       </div>
 
-      <div className='p-4 box mt-3 text-center'>
-        Already have an account? {' '}
-        <Link to='/'>Login</Link>
+      <div className="p-4 box mt-3 text-center">
+        Already have an account? <Link to="/">Login</Link>
       </div>
     </>
-  )
+  );
 }
 
-export default Signup
+export default Signup;

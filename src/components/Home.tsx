9d65d99e1529b7560df98bb0a6e24a562601710f
Mohasin <mohasin61@gmail.com>
2024-09-20 00:34:30 +0530

    migrate codebase to TS

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
-import {useUserAuth} from "@/context/UserAuthContext";
+import { useUserAuth } from "@/context/UserAuthContext";
 import auth from "@/firebase";
 
 function Home() {
